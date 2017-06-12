1be13ba83e884ac2f7627a0bd49b9270fed7e873
Sreenivasa Rajiv <sreenivasarajiv@gmail.com>
2017-06-12 20:06:06 +0530

    EJS Added
    EJS is added and parameter is passed from server.js to index.ejs

--- server.js
+++ server.js
@@ -2,15 +2,21 @@ import express from 'express';
 import config from './config';
 import apiRouter from './api/server'
 
-var server = express();
+const server = express();
+
+server.set('view engine', 'ejs');
 
 server.listen(config.port, () => {
     console.log(`listening to http://localhost:${config.port}`);
 });
 
-server.use(express.static('public'));
+// server.use(express.static('public'));
 server.use('/api', apiRouter);
 
 server.get('/', (req, res) => {
-    res.send("Hello from Express !!!");
+    // res.send("Hello from Express !!!");
+    // ejs index page
+    res.render('index',{
+        content : 'Hello, EJS by <strong>Sreenivasa Rajiv</strong>'
+    });
 });
